1f675aebf5e13c431b70a773f0ee72c8de7d5ae9
elenago <elenagomezcaballero@gmail.com>
2014-03-12 14:22:26 +0100

    cambios nuevos

---
+++ templates/header-bettina.php
@@ -0,0 +1,16 @@
+<header class="banner-bettina container" role="banner">
+  <div class="header-sup row">
+    <div class="lang may col-lg-7">idioma</div>
+    <div class="may col-lg-3">blog el vestido de bettina</div>
+    <div class="social pull-right col-lg-2">redes</div>
+  </div>
+
+  <div class="row">
+    <div class="font-header-inf may description col-lg-2">tu vestido.es</div>
+    <div class="font-header-inf may logo col-lg-5">bettina boutique</div>
+    <div class="menus-header col-lg-5">
+      <a class="pull-right brand" href="<?php echo home_url('/') ?>">Carrito compra</a>
+    </div>
+
+  </div>
+</header>
