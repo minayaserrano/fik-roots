e90efd737638947677a0c8dfcc983f553913d4a4
minayaserrano <minayaserrano@gmail.com>
2014-03-12 17:15:16 +0100

    Add cart menu

--- lib/init.php
+++ lib/init.php
@@ -9,9 +9,10 @@ function roots_setup() {
   // Register wp_nav_menu() menus (http://codex.wordpress.org/Function_Reference/register_nav_menus)
   register_nav_menus(array(
     'primary_navigation' => __('Primary Navigation', 'roots'),
-    'social-menu' => __( 'Top fixed Menu' ),
+    'social-menu' => __( 'Social Menu' ),
     'language-menu' => __( 'Language Menu' ),
     'blogshop-menu' => __( 'Blog Shop Menu' ),
+    'cart-menu' => __('Cart Menu'),
   ));
 
   // Add post thumbnails (http://codex.wordpress.org/Post_Thumbnails)
